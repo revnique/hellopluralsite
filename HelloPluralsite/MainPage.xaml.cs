a4378738bfe64037bc51b2abaf833bd5c963399f
revnique <revnique@gmail.com>
2015-11-02 00:55:52 -0600

    first working demo

---
+++ HelloPluralsite/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloPluralsite
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+		public void OnClick( object o, EventArgs e) {
+			message.Text = "goodbye pluralsite";
+		}
+	}
+}
